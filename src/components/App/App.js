4f2ade9d0bd8ff80ff2f01002dd980bc8996a641
Oksana-Zakhariia <okcahka.l@gmail.com>
2023-01-30 21:23:20 +0300

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,10 +12,6 @@ export function App() {
   const [bad, setBad] = useState(0);
   const options = ['good', 'neutral', 'bad'];
   const total = good + bad + neutral;
-  // const countTotalFeedback = () => {
-  //   const total = good + bad + neutral;
-  //   return total;
-  // };
 
   const positiveFeedback = parseInt((good * 100) / (good + bad + neutral));
 
@@ -55,54 +51,3 @@ export function App() {
     </AppContainer>
   );
 }
-// export class App extends React.Component {
-//   static defaultProps = {
-//     options: ['good', 'neutral', 'bad'],
-//   };
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   countTotalFeedback = () => {
-//     const { good, neutral, bad } = this.state;
-//     const total = good + bad + neutral;
-//     return total;
-//   };
-//   countPositiveFeedbackPercentage = () => {
-//     const { good, neutral, bad } = this.state;
-//     const positiveFeedback = parseInt((good * 100) / (good + bad + neutral));
-//     return positiveFeedback;
-//   };
-//   handleFeedback = option => {
-//     this.setState(prevState => ({
-//       [option]: prevState[option] + 1,
-//     }));
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     return (
-//       <AppContainer>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.handleFeedback}
-//           ></FeedbackOptions>
-//         </Section>
-//         <Section title="Statistics">
-//           {this.countTotalFeedback() > 0 ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positivePersentage={this.countPositiveFeedbackPercentage()}
-//             ></Statistics>
-//           ) : (
-//             <Notification message="There is no feedback"></Notification>
-//           )}
-//         </Section>
-//       </AppContainer>
-//     );
-//   }
